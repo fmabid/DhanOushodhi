4103fec34250292f2bab0e5ddc37ab763741e437
Abid <fmaabid@gmail.com>
2019-11-15 18:22:20 +0600

    offline detection

--- app/src/main/java/com/example/dhanoushodhi/DetectImageActivity.java
+++ app/src/main/java/com/example/dhanoushodhi/DetectImageActivity.java
@@ -1,13 +1,19 @@
 package com.example.dhanoushodhi;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,10 +23,31 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DetectImageActivity extends AppCompatActivity {
+
+    //Load the tensorflow inference library
+    static {
+        System.loadLibrary("tensorflow_inference");
+    }
+
+    //PATH TO OUR MODEL FILE AND NAMES OF THE INPUT AND OUTPUT NODES
+    private String MODEL_PATH = "file:///android_asset/mobilenetv2.pb";
+    private String INPUT_NAME = "input_2";
+    private String OUTPUT_NAME = "output_1";
+    private TensorFlowInferenceInterface tf;
+
+    //ARRAY TO HOLD THE PREDICTIONS AND FLOAT VALUES TO HOLD THE IMAGE DATA
+    float[] PREDICTIONS = new float[9];
+    private float[] floatValues;
+    private int[] INPUT_SIZE = {224,224,3};
+
 
     int PICK_IMAGE = 1;
     public ArrayList<Uri> imgUriList;
@@ -29,11 +56,15 @@ public class DetectImageActivity extends AppCompatActivity {
     Button buttonDetect;
     Button buttonDiseaseDetails;
     TextView tvDiseaseName;
+    TextView tvDiseaseNameOffline;
     ImageView thumbnail;
     LinearLayout layout;
 
     String diseaseName;
     String category;
+
+    Snackbar progressBar;
+    Bitmap bmp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,11 +78,17 @@ public class DetectImageActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
 
+        //initialize tensorflow with the AssetManager and the Model
+        tf = new TensorFlowInferenceInterface(getAssets(),MODEL_PATH);
+
+
         buttonGallery = findViewById(R.id.btn_gallery);
         buttonDetect = findViewById(R.id.btn_upload);
         buttonDiseaseDetails = findViewById(R.id.btn_details_disease);
-        tvDiseaseName = findViewById(R.id.tv_name);
+        tvDiseaseName = findViewById(R.id.tv_name);;
+        tvDiseaseNameOffline = findViewById(R.id.tv_name_prediction);
         layout = findViewById(R.id.result_container);
+        thumbnail = findViewById(R.id.iv_thumb);
 
         layout.setVisibility(View.GONE);
 
@@ -62,13 +99,99 @@ public class DetectImageActivity extends AppCompatActivity {
             }
         });
 
+
+        progressBar = Snackbar.make(thumbnail,"PROCESSING IMAGE",Snackbar.LENGTH_INDEFINITE);
+
         buttonDetect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                uploadImage();
+                //uploadImage();
+
+
+                progressBar.show();
+                predict(bmp);
             }
         });
     }
+
+    //FUNCTION TO COMPUTE THE MAXIMUM PREDICTION AND ITS CONFIDENCE
+    public Object[] argmax(float[] array){
+
+
+        int best = -1;
+        float best_confidence = 0.0f;
+
+        for(int i = 0;i < array.length;i++){
+
+            float value = array[i];
+
+            if (value > best_confidence){
+
+                best_confidence = value;
+                best = i;
+            }
+        }
+
+        return new Object[]{best,best_confidence};
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    public void predict(final Bitmap bitmap){
+        //Runs inference in background thread
+        new AsyncTask<Integer,Integer,Integer>(){
+            @Override
+            protected Integer doInBackground(Integer ...params){
+
+                //Resize the image into 224 x 224
+                Bitmap resized_image = ImageUtils.processBitmap(bitmap,224);
+
+                //Normalize the pixels
+                floatValues = ImageUtils.normalizeBitmap(resized_image,224,127.5f,1.0f);
+
+                //Pass input into the tensorflow
+                tf.feed(INPUT_NAME,floatValues,1,224,224,3);
+
+                //compute predictions
+                tf.run(new String[]{OUTPUT_NAME});
+
+                //copy the output into the PREDICTIONS array
+                tf.fetch(OUTPUT_NAME,PREDICTIONS);
+                System.out.println(Arrays.toString(PREDICTIONS));
+
+                //Obtained highest prediction
+                Object[] results = argmax(PREDICTIONS);
+
+
+                int class_index = (Integer) results[0];
+                float confidence = (Float) results[1];
+
+
+                try{
+                    final String conf = String.valueOf(confidence * 100).substring(0,5);
+                    //Convert predicted class index into actual label name
+                    final String label = ImageUtils.getLabel(getAssets().open("labels.json"),class_index);
+
+                    diseaseName = label;
+                    //Display result on UI
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Log.d("res", "run: ----->"+label);
+                            progressBar.dismiss();
+                            tvDiseaseNameOffline.setText("রোগের নামঃ " + label);
+                        }
+                    });
+                }
+                catch (Exception e){
+                }
+                return 0;
+            }
+        }.execute(0);
+
+
+    }
+
+
 
     public void chooseImage() {
         Intent intent = new Intent();
@@ -88,11 +211,11 @@ public class DetectImageActivity extends AppCompatActivity {
             imgUriList.add(uri);
 
             try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                bmp = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
                 // Log.d(TAG, String.valueOf(bitmap));
 
-                thumbnail = findViewById(R.id.iv_thumb);
-                thumbnail.setImageBitmap(bitmap);
+
+                thumbnail.setImageBitmap(bmp);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -101,11 +224,11 @@ public class DetectImageActivity extends AppCompatActivity {
 
     private void uploadImage() {
         if (imgUriList != null) {
-            try {
+            /*try {
                 Thread.sleep(5 * 1000);
             } catch (InterruptedException ie) {
                 Thread.currentThread().interrupt();
-            }
+            }*/
 
             diseaseName = "ধানের পাতা ব্লাস্ট";
             category = "রোগ";
